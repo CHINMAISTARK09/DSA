83119e16b35ca224e8c59ae7d709386666e47f48
CHINMAISTARK09 <chinmaichinnu111@gmail.com>
2026-03-12 11:35:13 +0530

    Create Array Duplicates.py

---
+++ Array Duplicates.py
@@ -0,0 +1,15 @@
+##Given an array arr[] of size n, containing elements from the range 1 to n, and each element appears at most twice, return an array of all the integers that appears twice.
+
+##Note: You can return the elements in any order but the driver code will print them in sorted order.
+
+class Solution:
+    def findDuplicates(self, arr):
+        result=[]
+        for i in range(len(arr)):
+            index=abs(arr[i])-1
+            if arr[index] <0:
+                result.append(abs(arr[i]))
+            else:
+                
+                arr[index]=-arr[index]
+        return result
