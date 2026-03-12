00b79d7229c918d89c5ae87d1289175ab7ed6201
CHINMAISTARK09 <chinmaichinnu111@gmail.com>
2026-03-12 11:16:01 +0530

    Update Find the number of islands.py

--- Find the number of islands.py
+++ Find the number of islands.py
@@ -1,3 +1,7 @@
+##Given a grid of size n*m (n is the number of rows and m is the number of columns in the grid) consisting of 'W's (Water) and 'L's (Land). Find the number of islands.
+
+##Note: An island is either surrounded by water or the boundary of a grid and is formed by connecting adjacent lands horizontally or vertically or diagonally i.e., in all 8 directions.
+
 class Solution:
     def makelandtowater(self,grid,i,j):
         if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j]=='W':
